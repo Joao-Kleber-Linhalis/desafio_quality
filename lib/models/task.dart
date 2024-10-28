1af85757b5d8c13dc10bb73794072bf349b2a335
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-10-28 09:17:31 -0300

    mock list

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -13,5 +13,12 @@ class Task {
     this.isDone = false,
   });
 
-
+  static List<Task> taskList() {
+    return [
+      Task(id: "1", taskText: "teste"),
+      Task(id: "2", taskText: "teste2"),
+      Task(id: "3", taskText: "teste3"),
+      Task(id: "4", taskText: "teste4"),
+    ];
+  }
 }
